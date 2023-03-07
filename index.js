73b2eb7427bc2c1ce5dc37940908c95a9440d70d
Brandon Barksdale <bbark93@gmail.com>
2023-03-07 08:49:27 -0500

    completed task 2

--- index.js
+++ index.js
@@ -77,7 +77,26 @@ class Person {
 */
 
 class Car {
-  
+  constructor(model, mpg) {
+   this.model = model;
+   this.milesPerGallon = mpg;
+   this.tank = 0;
+   this.odometer = 0;
+  }
+  fill(gallons) {
+    this.tank = this.tank + gallons;
+  }
+  drive(distance) {
+    const drivableMiles = this.tank + this.milesPerGallon;
+    if (distance <= drivableMiles) {
+      this.odometer = this.odometer + distance;
+      this.tank = this.tank - (distance / this.milesPerGallon);
+    } else {
+      this.odometer = this.odometer + drivableMiles;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer} miles!`
+    }
+  }
 }
 
 /*
